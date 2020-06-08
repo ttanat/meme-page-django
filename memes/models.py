cd5dfda894166ae5e38b540f5113b8ba6a67015a
Tanat <ttanat@protonmail.com>
2020-06-09 03:25:52 +0700

    New notifications model

--- memes/models.py
+++ memes/models.py
@@ -276,20 +276,25 @@ class Page(models.Model):
         super().delete(*args, **kwargs)
 
 
-# class Notification(models.Model):
-#     actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     action = models.CharField(max_length=64, blank=False)
-#     recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     link = models.URLField(max_length=64, blank=False)
-#     seen = models.BooleanField(default=False)
-#     message = models.CharField(max_length=128, blank=True)
-#     timestamp = models.DateTimeField(auto_now=False, default=timezone.now)
+class Notification(models.Model):
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    action = models.CharField(max_length=32, blank=False)
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
+    link = models.URLField(max_length=64, blank=False)
+    seen = models.BooleanField(default=False)
+    image = models.URLField(max_length=128, blank=True, default="")
+    message = models.CharField(max_length=128, blank=True)
+    timestamp = models.DateTimeField(auto_now=False, default=timezone.now)
 
-#     class Meta:
-#         ordering = ["-id"]
+    target_meme = models.ForeignKey(Meme, on_delete=models.CASCADE, null=True, blank=True)
+    target_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
+    target_page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True, blank=True)
 
-#     def __str__(self):
-#         return f"{self.message}"
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return f"{self.message}"
 
 
 class SubscribeRequest(models.Model):
