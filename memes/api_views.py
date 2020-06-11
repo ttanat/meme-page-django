12c642cbc7ad97274b4b7616a8a294186dfff9ae
Tanat <ttanat@protonmail.com>
2020-06-12 04:48:01 +0700

    Refactor code and check pathname for errors

--- memes/api_views.py
+++ memes/api_views.py
@@ -1,19 +1,16 @@
+from django.db.models import F, Q, Count
+from django.shortcuts import get_object_or_404
+
 from .serializers import *
+from .models import *
+from .utils import UOC, CATEGORIES
+
 from rest_framework import viewsets, pagination, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import *
-from .utils import CATEGORIES
-from django.db.models import F, Q, Count, Sum
-# from django.db.models.functions import Coalesce
-from django.http import Http404
-from django.core.exceptions import PermissionDenied
+from rest_framework.exceptions import NotAuthenticated, ParseError, PermissionDenied, NotFound
+
 from re import findall
-from django.shortcuts import get_object_or_404
-# from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.decorators import action
-from rest_framework.views import APIView
-from rest_framework.exceptions import NotAuthenticated
 
 
 class MemePagination(pagination.PageNumberPagination):
@@ -32,13 +29,17 @@ class MemeViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         memes = Meme.objects.annotate(username=F("user__username")).filter(hidden=False)
-        if not self.request.user.is_authenticated or (self.request.user.is_authenticated and not self.request.user.show_nsfw):
+
+        if (not self.request.user.is_authenticated or
+                (self.request.user.is_authenticated and not self.request.user.show_nsfw)):
             memes = memes.filter(nsfw=False)
-        pathname = self.request.query_params.get("p")
+
+        pathname = self.request.query_params.get("p", "")
 
         # Don't show page name in container header if user is in a meme page
-        if pathname[:5] != "page/":
+        if not pathname.startswith("page/"):
             memes = memes.annotate(pname=F("page__name"), pdname=F("page__display_name"))
+
         # Don't show memes from private pages
         if pathname != "feed":
             memes = memes.exclude(page__private=True)
@@ -46,21 +47,35 @@ class MemeViewSet(viewsets.ReadOnlyModelViewSet):
         if not pathname:
             return memes    # Currently just showing all memes
             # return memes.filter(uploaded__gte=timezone.now()-timedelta(3)).order_by("points")
+
         elif pathname == "feed":
             # Show memes from followed users and subscribed pages
             # Don't show memes from private pages that user is not subscribed to
             if self.request.user.is_authenticated:
                 return memes.filter(Q(user__followers=self.request.user)|Q(page__subscribers=self.request.user)) \
                             .exclude(Q(page__private=True)&~Q(page__subscribers=self.request.user))
+
             raise NotAuthenticated()
+
         elif pathname == "all":
             return memes
-        elif pathname[:5] == "page/":
-            return memes.filter(page__name=pathname[5:])
-        elif pathname[:7] == "browse/":
-            if pathname[7:] not in CATEGORIES:
-                raise Http404
-            return memes.filter(category__name=pathname[7:])
+
+        elif pathname.startswith("page/"):
+            pname = pathname.partition("page/")[2]
+
+            if not pname or any(c not in UOC for c in pname):
+                raise NotFound
+
+            return memes.filter(page__name=pname)
+
+        elif pathname.startswith("browse/"):
+            category_name = pathname.partition("browse/")[2]
+
+            if category_name not in CATEGORIES:
+                raise NotFound
+
+            return memes.filter(category__name=category_name)
+
         elif pathname == "search":
             query = self.request.query_params.get("q", "")[:64].strip()
             if query:
@@ -70,22 +85,27 @@ class MemeViewSet(viewsets.ReadOnlyModelViewSet):
                     for tag in tags:
                         q |= Q(tags__name__iexact=tag)
 
-                    return memes.filter(q).distinct()[:15] # Return search by tags
+                    return memes.filter(q).distinct() # Return search by tags
                 else:
-                    return memes.filter(caption__icontains=query).distinct()[:15] # Return search by caption
+                    return memes.filter(caption__icontains=query).distinct() # Return search by caption
 
-        raise Http404
+        raise NotFound
 
 
 class PrivateMemeViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = MemeSerializer
     pagination_class = MemePagination
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        name = self.request.query_params.get("n")
-        page = get_object_or_404(Page, name=name)
-        if not page.subscribers.filter(pk=self.request.user.pk).exists() and page.admin != self.request.user:
+        if "n" not in self.request.query_params:
+            raise ParseError
+
+        page = get_object_or_404(Page.objects.only("id", "admin_id"), name=self.request.query_params["n"])
+
+        if not page.subscribers.filter(pk=self.request.user.pk).exists() and page.admin_id != self.request.user.id:
             raise PermissionDenied
+
         return Meme.objects.annotate(username=F("user__username")).filter(page=page, hidden=False)
 
 
@@ -107,9 +127,11 @@ class CommentViewSet(viewsets.ReadOnlyModelViewSet):
     ordering = ["-id"]
 
     def get_queryset(self):
-        uuid = self.request.query_params.get("u")
+        if "u" not in self.request.query_params:
+            raise ParseError
+
         return Comment.objects.annotate(username=F("user__username"), num_replies=Count("replies", distinct=True)) \
-                              .filter(reply_to__isnull=True, meme__uuid=uuid)
+                              .filter(reply_to__isnull=True, meme__uuid=self.request.query_params["u"])
 
 
 class CommentFullViewSet(CommentViewSet):
@@ -117,9 +139,11 @@ class CommentFullViewSet(CommentViewSet):
     ordering_fields = ["id", "points"]
 
     def get_queryset(self):
-        uuid = self.request.query_params.get("u")
+        if "u" not in self.request.query_params:
+            raise ParseError
+
         return Comment.objects.annotate(username=F("user__username"), num_replies=Count("replies", distinct=True)) \
-                              .filter(reply_to__isnull=True, meme__uuid=uuid)
+                              .filter(reply_to__isnull=True, meme__uuid=self.request.query_params["u"])
 
 
 class ReplyPagination(CommentPagination):
@@ -129,12 +153,12 @@ class ReplyPagination(CommentPagination):
 class ReplyViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ReplySerializer
     pagination_class = ReplyPagination
-    # ordering_fields = ["id"]
-    # ordering = ["id"]
 
     def get_queryset(self):
-        uuid = self.request.query_params["u"]
-        return Comment.objects.annotate(username=F("user__username")).filter(reply_to__uuid=uuid).order_by("id")
+        if "u" not in self.request.query_params:
+            raise ParseError
+
+        return Comment.objects.annotate(username=F("user__username")).filter(reply_to__uuid=self.request.query_params["u"]).order_by("id")
 
 
 class SearchListPagination(pagination.PageNumberPagination):
