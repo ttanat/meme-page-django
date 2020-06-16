49ad3fe6b5a5e05d1b9cd8be61c39359d21168ad
Tanat <ttanat@protonmail.com>
2020-06-16 15:26:23 +0700

    Limit rate for anonymous users

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,10 +148,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle'
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'user': '60/minute'
+        'user': '60/minute',
+        'anon': '45/minute',
     }
 }
 
