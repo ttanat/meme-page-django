a69ffd0b3001004b2acc11cd6a75e8f607dda195
Tanat <ttanat@protonmail.com>
2020-06-26 15:10:02 +0700

    New analytics app

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'memes.apps.MemesConfig',
+    'analytics.apps.AnalyticsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
