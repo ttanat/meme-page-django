460817c1461c9fad1ea1bc11b38675ad45f9a3a3
Tanat <ttanat@protonmail.com>
2020-06-26 15:11:07 +0700

    New View model for analytics

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class View(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True, blank=True)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content_object} viewed on {self.timestamp}"
