cee8d03e803cd33486f03f7b9b67418e38369d1d
Tanat <ttanat@protonmail.com>
2020-08-05 15:15:19 +0700

    New notifications app

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'memes.apps.MemesConfig',
     'analytics.apps.AnalyticsConfig',
+    'notifications.apps.NotificationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
