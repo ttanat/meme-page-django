200efdcb80ecbdfcdac6e43a8a7e8275a58db631
Tanat <ttanat@protonmail.com>
2020-08-05 22:39:14 +0700

    New notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class Notification(models.Model):
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    action = models.CharField(max_length=32, blank=False)
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
+    link = models.URLField(max_length=64, blank=False)
+    seen = models.BooleanField(default=False)
+    image = models.URLField(max_length=128, blank=True, default="")
+    message = models.CharField(max_length=128, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True, blank=True)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return f"{self.message}"
