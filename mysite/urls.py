afa9d5ed8412c655cb2b85c0ecdb52f1fe7979b5
Tanat <ttanat@protonmail.com>
2020-08-18 11:48:56 +0700

    New view function for getting trending tags

--- mysite/urls.py
+++ mysite/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/analytics/', include("analytics.urls")),
     path('api/notifications/', include("notifications.urls")),
     path('admin/', admin.site.urls),
 ]
