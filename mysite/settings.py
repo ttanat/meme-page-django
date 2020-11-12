527f3f817339a0e76ec310db4ed3d4e4dfbea8dd
Tanat <ttanat@protonmail.com>
2020-11-12 14:29:59 +0000

    Change AWS region to London

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,7 +148,8 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 # AWS_S3_REGION_NAME = "us-east-2"
-AWS_S3_REGION_NAME = "ap-southeast-1"
+# AWS_S3_REGION_NAME = "ap-southeast-1"
+AWS_S3_REGION_NAME = "eu-west-2"
 
 
 # Login URL
