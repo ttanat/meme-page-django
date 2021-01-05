9c19882cdb3d1e3eabd76043a3730ba04debe687
Tanat <ttanat@protonmail.com>
2021-01-05 12:29:07 +0000

    Check username has at least one letter

--- memes/api/api_auth.py
+++ memes/api/api_auth.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 import re
+from string import ascii_letters
 
 
 @api_view(["POST"])
@@ -72,21 +73,36 @@ def register(request):
     email = request.POST["email"]
     password1 = request.POST["password1"]
 
+    # Check username, email, and password is submitted
     if not username or not email or not password1:
         error = "Username" if not username else "Email" if not email else "Password"
         return JsonResponse({"message": f"{error} cannot be blank", "field": error[0].lower()})
+
+    # Check length of username <= 32
     if len(username) > 32:
         return JsonResponse({"message": "Maximum 32 characters", "field": "u"})
+    # Check at least one letter [a-zA-Z] in username
+    if not any(c in username for c in ascii_letters) or not re.search(".*[a-zA-Z].*", username):
+        return JsonResponse({"message": "Username must have at least one letter", "field": "u"})
+    # Check username only contains valid characters
     if not re.search("^[a-zA-Z0-9_]+$", username):
         return JsonResponse({"message": "Invalid username", "field": "u"})
+
+    # Loose check for email (non-whitespace @ non-whitespace dot letters)
     if not re.search("^\S+@\S+\.[a-zA-Z]+$", email):
         return JsonResponse({"message": "Please enter a valid email", "field": "e"})
+
+    # Check password length is at least 6
     # if len(password1) < 6:
         # return JsonResponse({"message": "Password must be at least 6 characters", "field": "p"})
+    # Check password matches with confirm password
     if password1 != request.POST.get("password2"):
         return JsonResponse({"message": "Password does not match", "field": "p2"})
+
+    # Check if email already in use with active users
     if User.objects.filter(email=email, is_active=True).exists():
         return JsonResponse({"message": "Email already in use", "field": "e"})
+    # Case-insensitive check if username already exists
     if User.objects.filter(username__iexact=username).exists():
         return JsonResponse({"message": "Username already taken", "field": "u", "taken": True})
 
